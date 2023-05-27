03d13a163cf846e407d3d68a5004c7738b0bc60b
anhdat2003 <datktph26567@fpt.edu.vn>
2023-05-27 17:18:29 +0700

    chuc nang login- luu profile

--- Components/Khac.js
+++ Components/Khac.js
@@ -1,10 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheetView, Text, Button, Image, TouchableHighlight,StyleSheet,View } from 'react-native'
+import React, { useState } from 'react'
+import { useNavigation } from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Khac = (props) => {
+  const navigation = useNavigation();
+  const [objAccountInfo,setobjAccountInfo] = useState({});
+  const getAccountInfo = async()=>{
+    try {
+      const value = await AsyncStorage.getItem('Login');
+      console.log("Lấy dữ liệu" + value);
+      if(value !==null){
+        setobjAccountInfo(JSON.parse(value));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  /////
+  React.useEffect(() => {
+    const unsubscribe = props.navigation.addListener('focus', () => {
+        // khi màn hình được hiển thị sẽ gọi vào hàm này
+        // gọi hàm load dữ liệu
+        getAccountInfo();
+    });
+
+
+    return unsubscribe;
+}, [props.navigation]);
   return (
     <View>
       <Text onPress={()=>props.navigation.navigate("Login")}>Khac</Text>
+      <Text style={{fontSize:20,fontWeight:'bold',color:'#777777',padding:5,paddingLeft:10}}>1. {objAccountInfo.data.fullname}</Text>
     </View>
   )
 }
