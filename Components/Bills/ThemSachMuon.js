730313c2eb462b1cf3ad6254d0c9b36cb78d5bb6
khuatphilong2003 <khuatphilong2003@gmail.com>
2023-06-10 15:02:22 +0700

    search_addbook_bills

--- Components/Bills/ThemSachMuon.js
+++ Components/Bills/ThemSachMuon.js
@@ -5,14 +5,24 @@ import ItemThemSachMuon from './ItemThemSachMuon'
 import { API_URL } from '../../API__/api'
 
 const ThemSachMuon = ({navigation,route}) => {
- 
+  const [search,setSearch] = useState([]); 
   const [listBooks,setListBooks] = useState([]);
   const {GetData} = route.params;
   const getDataBook = ()=>{
-    const uri_list_books = API_URL+"books"
-    fetch(uri_list_books).then((res)=>{return res.json()}).then((res_json)=>{
-      setListBooks([...res_json.data])
-    })
+    if(search.length == 0){
+      const uri_list_books = API_URL+"books"
+      fetch(uri_list_books).then((res)=>{return res.json()}).then((res_json)=>{
+        setListBooks([...res_json.data])
+      })
+    }
+    else{
+      const uri = API_URL+"/books/search?name="+search;
+      fetch(uri).then((res)=>{return res.json()}).then((res_json)=>{
+        setListBooks([...res_json.data])
+      });
+
+    }
+    
   }
   useEffect(()=>{
     getDataBook();
@@ -22,10 +32,11 @@ const ThemSachMuon = ({navigation,route}) => {
       <View style={{flexDirection:'row',padding:10}}>
         
         <View style={{flex:1,backgroundColor:'white',marginRight:10,justifyContent:'center',paddingLeft:10,borderRadius:5}}>
-        <TextInput placeholder='Tìm kiếm'/>
+        <TextInput placeholder='Tìm kiếm' onChangeText={(Text)=>setSearch(Text)}/>
         </View>
         <TouchableOpacity
         style={{backgroundColor:color.xanh,justifyContent:'center',alignItems:'center',padding:10,borderRadius:5}}
+        onPress={getDataBook}
         >
           <Text style={{color:'white'}}>Tìm kiếm</Text>
         </TouchableOpacity>
